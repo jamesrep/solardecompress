e22295e16478009fbad2e89f2eb17a68b70025d4
james <wallparse@gmail.com>
2020-12-23 10:42:23 +0100

    Fixed bug not outputting multiple zipped lines
    Added feature

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿// James - 2020.
 // Just a simple c# code to decode the solarwinds stage 1 strings found in the fireye-compromise .
 // Can scan decompiled c#-source code files and decode the obfuscated strings.
-//
+// Example:
+// mono solardecompress2.exe --decodefile --regex --file /home/james/src/solarwinds/SolarWinds.Orion.Core.BusinessLayer/OrionImprovementBusinessLayer.cs > decoded_OrionImprovementBusinessLayer.cs
+
+
 using System;
 using System.IO;
 using System.IO.Compression;
@@ -20,7 +23,7 @@ namespace solardecompress
             {
                 using (MemoryStream memoryStream2 = new MemoryStream())
                 {
-                    using (DeflateStream deflateStream = new DeflateStream((Stream)memoryStream2, CompressionMode.Compress))  memoryStream1.CopyTo((Stream)deflateStream);
+                    using (DeflateStream deflateStream = new DeflateStream((Stream)memoryStream2, CompressionMode.Compress)) memoryStream1.CopyTo((Stream)deflateStream);
                     return memoryStream2.ToArray();
                 }
             }
@@ -47,7 +50,7 @@ namespace solardecompress
             {
                 return Convert.ToBase64String(Compress(Encoding.UTF8.GetBytes(input)));
             }
-            catch (Exception ex) 
+            catch (Exception ex)
             {
                 return "";
             }
@@ -95,46 +98,73 @@ namespace solardecompress
         {
             bool bModeDecode = true;
             bool bRegex = false;
+            bool bDecodeFileMode = false;
 
-            if(args.Length < 1)
+            if (args.Length < 1)
             {
                 Console.WriteLine("--file <filename>    - use file to search for strings to decode");
                 Console.WriteLine("--regex              - Scan inputs for regex");
                 Console.WriteLine("--encode             - Set encode mode");
                 Console.WriteLine("--decode             - Set decode mode (default)");
-                
+                Console.WriteLine("--decodefile         - Decode the whole file and change the source code output to stdout");
+
                 Console.WriteLine();
                 Console.WriteLine("Example: solardecompress --regex --file OrionImprovementBusinessLayer.cs");
 
                 return;
             }
 
-            for(int i=0; i  < args.Length; i++)
+            for (int i = 0; i < args.Length; i++)
             {
                 string strArg = args[i];
 
-                if(strArg.IndexOf("--file") == 0)
+                if (strArg.IndexOf("--file") == 0)
                 {
                     i++;
                     strArg = args[i];
 
                     string[] strAll = File.ReadAllLines(strArg);
 
-                    foreach(string str in strAll)
+                    foreach (string str in strAll)
                     {
                         string strCarved = str;
 
                         if (bRegex)
                         {
-                            Match mRex = Regex.Match(str, "\\(\\s{0,}\"(?<base64>[A-z|0-9|\\/|=]{1,})\"\\s{0,}");
+                            MatchCollection mRex = Regex.Matches(str, "\\(\\s{0,}\"(?<base64>[A-z|0-9|\\/|=]{1,})\"\\s{0,}");
 
-                            if (mRex.Success)
+                            if (mRex.Count > 0)
                             {
-                                for (int g = 0; g < mRex.Groups["base64"].Captures.Count; g++)
+                                string strDecodedLine = str;
+
+
+                                for (int g = 0; g < mRex.Count; g++)
                                 {
-                                    string strVal = mRex.Groups["base64"].Captures[g].Value;
-                                    printDecoded(bModeDecode, strVal);
+
+                                    string strVal = mRex[g].Groups["base64"].Captures[0].Value;
+
+
+                                    if (bDecodeFileMode)
+                                    {
+                                        string strToReplace = "OrionImprovementBusinessLayer.ZipHelper.Unzip(\"" + strVal + "\")";
+                                        string strDecoded = "\"" + ZHelperFoundInBackdoor.Unzip(strVal) + "\"";
+
+                                        strDecodedLine = strDecodedLine.Replace(strToReplace, strDecoded);
+                                    }
+                                    else
+                                    {
+                                        printDecoded(bModeDecode, strVal);
+                                    }
                                 }
+
+                                if (bDecodeFileMode)
+                                {
+                                    Console.WriteLine(strDecodedLine);
+                                }
+                            }
+                            else if(bDecodeFileMode)
+                            {
+                                Console.WriteLine(str);
                             }
                         }
                         else
@@ -146,6 +176,10 @@ namespace solardecompress
                 else if (strArg.IndexOf("--regex") == 0)
                 {
                     bRegex = true;
+                }
+                else if (strArg.IndexOf("--decodefile") == 0)
+                {
+                    bDecodeFileMode = true;
                 }
                 else if (strArg.IndexOf("--noregex") == 0)
                 {
